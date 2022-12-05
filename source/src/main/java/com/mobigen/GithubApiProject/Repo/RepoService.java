81ea7d49ce5a23b864403c80476c96c73f9ae6a1
leejeongkang <krj1226@gmail.com>
2022-12-05 10:24:28 +0900

    refactor: URL 클래스 생성

--- source/src/main/java/com/mobigen/GithubApiProject/Repo/RepoService.java
+++ source/src/main/java/com/mobigen/GithubApiProject/Repo/RepoService.java
@@ -14,7 +14,7 @@ public class RepoService {
 
     private final RestAPI restAPI;
     private final GithubApiToken githubApiToken;
-    static final String owner = "mobigen";
+    private final UrlService urlService;
     private List<Map<String, Object>> getName (JsonResult result) {
         List<Map<String, Object>> list = (List<Map<String, Object>>) result.getData();
 
@@ -28,67 +28,38 @@ public class RepoService {
         }
         return list;
     }
-    private String getURL(String repo, String key) {
-
-        final String githubURL = "https://api.github.com/repos/" + owner + "/" + repo;
-
-        final String repoListURL = "https://api.github.com/orgs/" + owner + "/repos";
-        final String branchListURL = githubURL + "/branches";
-        final String commitCountURL = githubURL + "/commits?per_page=100";
-        final String prCountURL = githubURL + "/pulls?state=all&per_page=100";
-        final String userCountURL = githubURL + "/contributors?state=all";
-        final String branchCountURL = githubURL + "/branches?state=all";
-        final String commitCountByUserURL = githubURL + "/commits?state=all";
-        final String prCountByUserURL = githubURL +"/pulls?state=all";
-        final String commitCountByDateURL = githubURL + "/commits?state=all";
-        final String prCountByDateURL = githubURL + "/pulls?state=all";
-
-        Map<String, String> stringMap = new HashMap<>();
-        stringMap.put("repoListURL", repoListURL);
-        stringMap.put("branchListURL", branchListURL);
-        stringMap.put("commitCountURL", commitCountURL);
-        stringMap.put("prCountURL", prCountURL);
-        stringMap.put("userCountURL", userCountURL);
-        stringMap.put("branchCountURL", branchCountURL);
-        stringMap.put("commitCountByUserURL", commitCountByUserURL);
-        stringMap.put("prCountByUserURL", prCountByUserURL);
-        stringMap.put("commitCountByDateURL", commitCountByDateURL);
-        stringMap.put("prCountByDateURL", prCountByDateURL);
-
-        return stringMap.get(key);
-    }
     public Object repoList() {
-        JsonResult result = this.restAPI.get(getURL(null,"repoListURL"),null, githubApiToken.accessToken());
+        JsonResult result = this.restAPI.get(urlService.getRepoListURL(),null, githubApiToken.accessToken());
 
         return getName(result);
     }
 
     public Object branchList(String repo) {
-        JsonResult result = this.restAPI.get(getURL(repo,"branchListURL"),null, githubApiToken.accessToken());
+        JsonResult result = this.restAPI.get(urlService.getBranchListURL(repo),null, githubApiToken.accessToken());
 
         return getName(result);
     }
 
     public Integer commitCount(String repo) {
-        JsonResult result = this.restAPI.get(getURL(repo,"commitCountURL"),null, githubApiToken.accessToken());
+        JsonResult result = this.restAPI.get(urlService.getCommitCountURL(repo),null, githubApiToken.accessToken());
 
         return getName(result).size();
     }
 
     public Integer prCount(String repo) {
-        JsonResult result = this.restAPI.get(getURL(repo,"prCountURL"),null, githubApiToken.accessToken());
+        JsonResult result = this.restAPI.get(urlService.getPrCountURL(repo),null, githubApiToken.accessToken());
 
         return getName(result).size();
     }
 
     public Integer userCount(String repo) {
-        JsonResult result = this.restAPI.get(getURL(repo,"userCount"),null, githubApiToken.accessToken());
+        JsonResult result = this.restAPI.get(urlService.getUserCountURL(repo),null, githubApiToken.accessToken());
 
         return getName(result).size();
     }
 
     public Integer branchCount(String repo) {
-        JsonResult result = this.restAPI.get(getURL(repo,"branchCount"),null, githubApiToken.accessToken());
+        JsonResult result = this.restAPI.get(urlService.getBranchCountURL(repo),null, githubApiToken.accessToken());
 
         return getName(result).size();
     }
@@ -97,7 +68,7 @@ public class RepoService {
         Map<String, String> userMap =  new HashMap<>();
         userMap.put("author", user);
 
-        JsonResult result = this.restAPI.get(getURL(repo,"commitCountByUserURL"), userMap, githubApiToken.accessToken());
+        JsonResult result = this.restAPI.get(urlService.getCommitCountByUserURL(repo), userMap, githubApiToken.accessToken());
 
         return getName(result).size();
     }
@@ -105,7 +76,7 @@ public class RepoService {
         Map<String, String> userMap = new HashMap<>();
         userMap.put("author", user);
 
-        JsonResult result = this.restAPI.get(getURL(repo,"prCountByUser"), userMap, githubApiToken.accessToken());
+        JsonResult result = this.restAPI.get(urlService.getPrCountByUserURL(repo), userMap, githubApiToken.accessToken());
 
         return getName(result).size();
     }
@@ -113,7 +84,7 @@ public class RepoService {
     public Integer commitCountByDate(String repo, String datePick) {
         Map<String, String> dateMap = getDate(datePick);
 
-        JsonResult result = restAPI.get(getURL(repo,"commitCountByDateURL"), dateMap, githubApiToken.accessToken());
+        JsonResult result = restAPI.get(urlService.getCommitCountByDateURL(repo), dateMap, githubApiToken.accessToken());
 
         return getName(result).size();
     }
@@ -121,7 +92,7 @@ public class RepoService {
     public Integer prCountByDate(String repo, String datePick) {
         Map<String, String> dateMap = getDate(datePick);
 
-        JsonResult result = restAPI.get(getURL(repo, "prCountByDateURL"), dateMap, githubApiToken.accessToken());
+        JsonResult result = restAPI.get(urlService.getPrCountByDateURL(repo), dateMap, githubApiToken.accessToken());
 
         return getName(result).size();
     }
