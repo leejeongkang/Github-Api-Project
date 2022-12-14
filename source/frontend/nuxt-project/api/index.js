47b65f44e44d89dead075ac191f0cc56781b849d
leejeongkang <krj1226@gmail.com>
2022-12-14 13:17:41 +0900

    feat: API URL 분리

--- source/frontend/nuxt-project/api/index.js
+++ source/frontend/nuxt-project/api/index.js
@@ -3,9 +3,46 @@ import axios from 'axios'
 const instance = axios.create({
   baseURL: 'http://localhost:80/repo-api/'
 })
-
+instance.interceptors.request.use(
+  (config) => {
+    config.headers['Access-Control-Allow-Origin'] = '*';
+    return config
+  }
+)
 function fetchRepos() {
   return instance.get('repos')
 }
+function fetchBranchList(repo) {
+  return instance.get(`branch-list/${repo}`, {repo})
+}
 
-export { fetchRepos }
+function fetchCommitCnt(repo) {
+  return instance.get(`${repo}/commit-cnt`, {repo})
+}
+
+function fetchAuthorCnt(repo) {
+  return instance.get(`${repo}/user-cnt`, {repo})
+}
+
+function fetchPrCnt(repo) {
+  return instance.get(`${repo}/pr-cnt`, {repo})
+}
+
+function fetchBranchCnt(repo) {
+  return instance.get(`${repo}/branch-cnt`, {repo})
+}
+
+function fetchCommitCntByUser(repo, user){
+  return instance.get(`${repo}/commit-cnt/${user}`, {repo, user})
+}
+function fetchPrCntByUser(repo, user){
+  return instance.get(`${repo}/pr-cnt/${user}`, {repo, user})
+}
+function fetchCommitCntByDate(repo, datePick){
+  return instance.get(`${repo}/commit-cnt/${datePick}`, {repo, datePick})
+}
+function fetchPrCntByDate(repo, datePick){
+  return instance.get(`${repo}/pr-cnt/${datePick}`, {repo, datePick})
+}
+export { fetchRepos, fetchBranchList, fetchAuthorCnt, fetchBranchCnt, fetchCommitCnt, fetchPrCnt, fetchPrCntByDate,
+          fetchCommitCntByDate, fetchCommitCntByUser, fetchPrCntByUser}
