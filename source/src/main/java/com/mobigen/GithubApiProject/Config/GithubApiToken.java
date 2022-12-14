05e62f740dcdef7bd3183fe5b04414f9fe62d45e
leejeongkang <krj1226@gmail.com>
2022-12-14 13:12:31 +0900

    refactor: Github API Token 만료 -> 재발급

--- source/src/main/java/com/mobigen/GithubApiProject/Config/GithubApiToken.java
+++ source/src/main/java/com/mobigen/GithubApiProject/Config/GithubApiToken.java
@@ -1,6 +1,5 @@
 package com.mobigen.GithubApiProject.Config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -10,8 +9,8 @@ import java.util.Map;
 public class GithubApiToken {
 
     //final String token ="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";
-    final String token = "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";
-
+    //final String token = "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";
+    final String token ="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";
     public Map<String, String> accessToken() {
 
         Map<String, String> accessToken = new HashMap<>();
