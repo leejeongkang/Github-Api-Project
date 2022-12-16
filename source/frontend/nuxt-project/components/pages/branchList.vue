005f13b017f2e0a3b5aa6d893561bf9c5bcfb3a9
leejeongkang <krj1226@gmail.com>
2022-12-16 18:43:11 +0900

    refactor: 컴포넌트 분리

---
+++ source/frontend/nuxt-project/components/pages/branchList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>branch list component</h3>
+    <p> {{ repo }}</p>
+    <ul>
+      <li v-for="branch in $store.state.branchList"
+          :key="branch.name">
+        {{ branch.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "BranchList",
+  props: {
+    repo: {
+      type: String,
+      required: true
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ source/frontend/nuxt-project/components/pages/repoList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="repo in $store.state.repos"
+        :key="repo.name"
+        @click="fetch(repo.name)">
+        <b>{{ repo.name }}</b>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "repoList",
+  async fetch() {
+    await this.$store.dispatch("getRepos")
+  },
+  data() {
+    return{
+      repo: ''
+    }
+  },
+  methods: {
+    async fetch(repo){
+      await this.$store.dispatch('getBranchList', repo)
+      await this.$store.dispatch('getCommitCnt', repo)
+      await this.$store.dispatch('getPrCnt', repo)
+      await this.$store.dispatch('getUserCnt', repo)
+      await this.$store.dispatch('getBranchCnt', repo)
+      this.$emit('update',repo)
+    },
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
