b7d7d1f77fec74e02b65a2cfc80203348d3d5aec
leejeongkang <krj1226@gmail.com>
2022-12-16 18:46:27 +0900

    refactor: 레이아웃, css 퍼블리싱 간략화

--- source/frontend/nuxt-project/pages/index.vue
+++ source/frontend/nuxt-project/pages/index.vue
@@ -1,41 +1,73 @@
 <template>
   <div>
-    <ul>
-      <li v-for="repo in $store.state.repos" :key="repo.name" @click="fetch(repo.name)">
-        <b>{{ repo.name }}</b>
-      </li>
-    </ul>
-    <ul>
-      <li v-for="branch in $store.state.branchList" :key="branch.name">
-        {{ branch.name }}
-      </li>
-    </ul>
+    <div class="repo">
+      <repo-list @update="timeChanged"/>
+    </div>
+    <div class="branch">
+      <branch-list :repo="repo"></branch-list>
+    </div>
+    <div class="main">
+      <detail-count :repo="repo"></detail-count>
+      <date-picker :repo="repo" @update="dateUpdate"></date-picker>
+      <date-count :repo="repo" :date-pick="datePick"></date-count>
+      <highcharts />
+    </div>
   </div>
 </template>
 
 
 <script>
-import {
-  FETCH_BRANCH_LIST,
-  FETCH_REPOS
-} from "@/store";
-import DetailCount from "@component/pages/detailCount";
+import BranchList from "@component/pages/branchList";
+import DetailCount from "@component/pages/count";
+import DatePicker from '@component/functional/datepicker/date-picker'
+import RepoList from "@component/pages/repoList";
+import Highcharts from "@functional/highcharts/hightcharts";
+import DateCount from "@component/pages/countByDate";
+import {mapActions} from "vuex";
 
 export default {
   name: "Index",
-  components: {DetailCount},
-  async asyncData({ store }) {
-    await store.dispatch(FETCH_REPOS)
+  components: {DateCount, Highcharts, BranchList, RepoList, DetailCount, DatePicker,},
+  data() {
+    return{
+      repo: '',
+      datePick: ''
+    }
   },
   methods: {
-    async fetch(repo){
-      console.log(repo)
-      await this.$store.dispatch(FETCH_BRANCH_LIST, repo)
-    }
+    timeChanged(repo) {
+      this.repo = repo
+    },
+    dateUpdate(dateValue) {
+      this.datePick = dateValue
+      console.log(this.datePick)
+    },
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.repo {
+  float: left;
+  width: 13%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: lightgray;
+  padding: 8px 12px;
+}
+.branch {
+  float: left;
+  width: 13%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background:lightblue;
+  padding: 8px 12px;
+}
+.main {
+  float: left;
+  width: 73%
+}
 
 </style>
