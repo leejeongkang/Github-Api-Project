1394dc387cc8182efbc7f55d4ff1e73cc651c41f
leejeongkang <krj1226@gmail.com>
2022-12-16 18:44:37 +0900

    [IDS-1744] feat: 커밋, 피알, 브랜치, 유저 갯수 출력

---
+++ source/frontend/nuxt-project/components/pages/count/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Commit, Author, Branches, PullRequest Count</h2>
+    <p>commit {{ $store.state.commitCnt }}</p>
+    <p>pr {{ $store.state.prCnt }}</p>
+    <p>author {{ $store.state.authorCnt }}</p>
+    <p>branch {{ $store.state.branchCnt }}</p>
+    <p>props : {{ repo }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "detailCount",
+  props: {
+    repo: {
+      type: String
+    }
+  },
+}
+</script>
