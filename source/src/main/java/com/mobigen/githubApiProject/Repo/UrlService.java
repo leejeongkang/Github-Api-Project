740b7a46bfa5f0e7f526aae2dcc6a55679240234
leejeongkang <krj1226@gmail.com>
2022-12-22 10:57:36 +0900

    refactor: 상수명 수정

--- source/src/main/java/com/mobigen/githubApiProject/Repo/UrlService.java
+++ source/src/main/java/com/mobigen/githubApiProject/Repo/UrlService.java
@@ -6,47 +6,47 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class UrlService {
-    static final String owner = "mobigen";
-    static final String githubURL = "https://api.github.com/repos/" + owner + "/";
-    static final String repoListURL = "https://api.github.com/orgs/" + owner + "/repos?per_page=100";
-    static final String branchListURL = "/branches";
-    static final String commitCountURL = "/commits?per_page=100";
-    static final String prCountURL = "/pulls?state=all&per_page=100";
-    static final String userCountURL = "/contributors?state=all";
-    static final String branchCountURL = "/branches?state=all";
-    static final String commitCountByUserURL = "/commits?per_page=100";
-    static final String prCountByUserURL = "/pulls?state=all&per_page=100";
-    static final String commitCountByDateURL = "/commits?per_page=100&since=";
-    static final String prCountByDateURL = "/pulls?state=all&per_page=100";
+    static final String OWNER = "mobigen";
+    static final String GITHUB_URL = "https://api.github.com/repos/" + OWNER + "/";
+    static final String REPO_LIST_URL = "https://api.github.com/orgs/" + OWNER + "/repos?per_page=100";
+    static final String BRANCH_LIST_URL = "/branches";
+    static final String COMMIT_COUNT_URL = "/commits?per_page=100";
+    static final String PR_COUNT_URL = "/pulls?state=all&per_page=100";
+    static final String USER_COUNT_URL = "/contributors?state=all";
+    static final String BRANCH_COUNT_URL = "/branches?state=all";
+    static final String COMMIT_COUNT_BY_USER_URL = "/commits?per_page=100";
+    static final String PR_COUNT_BY_USER_URL = "/pulls?state=all&per_page=100";
+    static final String COMMIT_COUNT_BY_DATE_URL = "/commits?per_page=100&since=";
+    static final String PR_COUNT_BY_DATE_URL = "/pulls?state=all&per_page=100";
 
     public String getRepoListURL() {
-        return repoListURL;
+        return REPO_LIST_URL;
     }
     public String getBranchListURL(String repo) {
-        return githubURL + repo + branchListURL;
+        return GITHUB_URL + repo + BRANCH_LIST_URL;
     }
     public String getCommitCountURL(String repo) {
-        return githubURL + repo + commitCountURL;
+        return GITHUB_URL + repo + COMMIT_COUNT_URL;
     }
     public String getPrCountURL(String repo) {
-        return githubURL + repo + prCountURL;
+        return GITHUB_URL + repo + PR_COUNT_URL;
     }
     public String getUserCountURL(String repo) {
-        return githubURL + repo + userCountURL;
+        return GITHUB_URL + repo + USER_COUNT_URL;
     }
     public String getBranchCountURL(String repo) {
-        return githubURL + repo + branchCountURL;
+        return GITHUB_URL + repo + BRANCH_COUNT_URL;
     }
     public String getCommitCountByUserURL(String repo) {
-        return githubURL + repo + commitCountByUserURL;
+        return GITHUB_URL + repo + COMMIT_COUNT_BY_USER_URL;
     }
     public String getPrCountByUserURL(String repo) {
-        return githubURL + repo + prCountByUserURL;
+        return GITHUB_URL + repo + PR_COUNT_BY_USER_URL;
     }
     public String getCommitCountByDateURL(String repo, String dateValue) {
-        return githubURL + repo + commitCountByDateURL + dateValue;
+        return GITHUB_URL + repo + COMMIT_COUNT_BY_DATE_URL + dateValue;
     }
     public String getPrCountByDateURL(String repo) {
-        return githubURL + repo + prCountByDateURL;
+        return GITHUB_URL + repo + PR_COUNT_BY_DATE_URL;
     }
 }
